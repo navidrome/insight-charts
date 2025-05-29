d0b5b97b9d7ff6b19e6867852f3af79af3e5aabf
Caio Cotts <caio@cotts.com.br>
2025-05-29 17:15:46 -0400

    implement file system pie chart

---
+++ generators.ts
@@ -0,0 +1,162 @@
+import { Summary } from './model/index.ts'
+import { DatabaseSync } from 'node:sqlite'
+
+const vegaSchema = 'https://vega.github.io/schema/vega-lite/v5.json'
+
+const osPie = (db: DatabaseSync): string => {
+  const summary: Summary = JSON.parse(
+    ((row) =>
+      row && row['data'] instanceof Uint8Array
+        ? new TextDecoder().decode(row['data'])
+        : '')(
+        db.prepare('select data from summary order by time desc').get(),
+      ),
+  )
+
+  const values = Object.entries(summary.os ?? {}).map(
+    ([name, count]) => ({ os: name, c: count }),
+  )
+  return JSON.stringify({
+    $schema: vegaSchema,
+    data: {
+      values: values,
+    },
+    mark: {
+      type: 'arc',
+      tooltip: true,
+    },
+    encoding: {
+      theta: {
+        field: 'c',
+        type: 'quantitative',
+        stack: 'normalize',
+        title: 'Percentage',
+      },
+      color: {
+        field: 'os',
+        type: 'nominal',
+        sort: {
+          field: 'c',
+          order: 'descending',
+        },
+        title: 'Operating System',
+      },
+      order: {
+        field: 'c',
+        type: 'quantitative',
+        sort: 'descending',
+        title: 'Count',
+      },
+    },
+  })
+}
+
+const numInstanceLine = (db: DatabaseSync): string => {
+  const stmt = db.prepare(
+    "select time, data from summary where data not like '{}' and time > '2024-12-21'",
+  )
+  const values = [{}]
+  for (const row of stmt.iterate()) {
+    const dt = new Date(row.time as string).toISOString().slice(0, 10)
+    const s: Summary = JSON.parse(
+      new TextDecoder().decode(row.data as Uint8Array),
+    )
+
+    Object.entries(s.versions ?? {}).forEach(
+      ([version, count]) => values.push({ v: version, n: count, d: dt }),
+    )
+    values.push({
+      v: 'all',
+      n: s.numInstances,
+      d: dt,
+    })
+  }
+  return JSON.stringify({
+    $schema: vegaSchema,
+    data: {
+      values: values,
+    },
+    height: 500,
+    width: 1000,
+    mark: {
+      type: 'line',
+      tooltip: true,
+      point: true,
+    },
+    encoding: {
+      color: {
+        field: 'v',
+        type: 'nominal',
+        sort: {
+          field: 'n',
+          op: 'sum',
+          order: 'descending',
+        },
+        title: 'Version',
+      },
+      x: {
+        field: 'd',
+        type: 'temporal',
+        timeUnit: 'yearmonthdate',
+        title: 'Date',
+      },
+      y: {
+        field: 'n',
+        type: 'quantitative',
+        title: 'Number of Instances',
+      },
+    },
+  })
+}
+
+const fsPie = (db: DatabaseSync): string => {
+  const summary: Summary = JSON.parse(
+    ((row) =>
+      row && row['data'] instanceof Uint8Array
+        ? new TextDecoder().decode(row['data'])
+        : '')(
+        db.prepare('select data from summary order by time desc').get(),
+      ),
+  )
+  const values = Object.entries(summary.musicFS ?? {}).map(
+    ([type, count]) => ({ fs: type, c: count }),
+  )
+
+  return JSON.stringify(
+    {
+      $schema: vegaSchema,
+      data: {
+        values: values,
+      },
+      mark: {
+        type: 'arc',
+        tooltip: true,
+      },
+      encoding: {
+        theta: {
+          field: 'c',
+          type: 'quantitative',
+          stack: 'normalize',
+          title: 'Percentage',
+        },
+        color: {
+          field: 'fs',
+          type: 'nominal',
+          sort: {
+            field: 'c',
+            order: 'descending',
+          },
+          title: 'File System',
+        },
+        order: {
+          field: 'c',
+          type: 'quantitative',
+          sort: 'descending',
+          title: 'Count',
+        },
+      },
+    },
+  )
+}
+
+export { fsPie, numInstanceLine, osPie }
