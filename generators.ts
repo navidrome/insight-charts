33c49cc5d750b5e1861391977765352621a23937
Caio Cotts <caio@cotts.com.br>
2025-06-18 20:06:09 -0400

    consolidate pie chart generation logic also redesign some of the charts

--- generators.ts
+++ generators.ts
@@ -3,6 +3,71 @@ import { StatementSync } from 'node:sqlite'
 
 const vegaSchema = 'https://vega.github.io/schema/vega-lite/v5.json'
 
+const genPie = (
+  width: number | string,
+  height: number | string,
+  colorField: string,
+  tooltipTitle: string,
+) => ({
+  $schema: vegaSchema,
+  background: null,
+  height: height,
+  width: width,
+  transform: [
+    {
+      joinaggregate: [
+        { op: 'sum', field: 'c', as: 'total' },
+      ],
+    },
+    {
+      calculate:
+        "datum.c / datum.total < 0.01 ? '<1%' : format(datum.c / datum.total, '.0%')",
+      as: 'pctLabel',
+    },
+  ],
+  mark: {
+    type: 'arc',
+    tooltip: true,
+  },
+  encoding: {
+    theta: {
+      field: 'c',
+      type: 'quantitative',
+      stack: 'normalize',
+      title: 'Percentage',
+    },
+    color: {
+      legend: {
+        labelLimit: 250,
+        symbolLimit: 13,
+      },
+      field: colorField,
+      type: 'nominal',
+      sort: {
+        field: 'c',
+        order: 'descending',
+      },
+      title: '',
+    },
+    order: {
+      field: 'c',
+      type: 'quantitative',
+      sort: 'descending',
+      title: 'Count',
+    },
+    tooltip: [
+      { field: colorField, type: 'nominal', title: tooltipTitle },
+      {
+        field: 'c',
+        type: 'quantitative',
+        title: 'Count',
+        format: ',',
+      },
+      { field: 'pctLabel', type: 'nominal', title: 'Percentage' },
+    ],
+  },
+})
+
 const parseSummaryData = (stmt: StatementSync) =>
   JSON.parse(
     ((row) =>
@@ -18,43 +83,70 @@ const osPie = (stmt: StatementSync): string => {
   const values = Object.entries(summary.os ?? {}).map(
     ([name, count]) => ({ os: name, c: count }),
   )
+
   return JSON.stringify({
-    $schema: vegaSchema,
     title: 'Operating Systems',
     description: 'Distribution of operating systems used by clients',
     data: {
       values: values,
     },
-    mark: {
-      type: 'arc',
-      tooltip: true,
-    },
-    encoding: {
-      theta: {
-        field: 'c',
-        type: 'quantitative',
-        stack: 'normalize',
-        title: 'Percentage',
-      },
-      color: {
-        field: 'os',
-        type: 'nominal',
-        sort: {
-          field: 'c',
-          order: 'descending',
-        },
-        title: 'Operating System',
-      },
-      order: {
-        field: 'c',
-        type: 'quantitative',
-        sort: 'descending',
-        title: 'Count',
-      },
-    },
+    ...genPie('container', 420, 'os', 'OS'),
   })
 }
 
+const musicFsPie = (stmt: StatementSync): string => {
+  const summary: Summary = parseSummaryData(stmt)
+  const values = Object.entries(summary.musicFS ?? {}).map(
+    ([type, count]) => ({ fs: type, c: count }),
+  )
+
+  return JSON.stringify(
+    {
+      title: 'Music File Systems',
+      description: 'Distribution of file systems used for music files',
+      data: {
+        values: values,
+      },
+      ...genPie('container', 420, 'fs', 'File System'),
+    },
+  )
+}
+
+const dataFsPie = (stmt: StatementSync): string => {
+  const summary: Summary = parseSummaryData(stmt)
+  const values = Object.entries(summary.dataFS ?? {}).map(
+    ([type, count]) => ({ fs: type, c: count }),
+  )
+
+  return JSON.stringify(
+    {
+      title: 'Data File Systems',
+      description: 'Distribution of file systems used for data files',
+      data: {
+        values: values,
+      },
+      ...genPie('container', 420, 'fs', 'File System'),
+    },
+  )
+}
+
+const playerTypePie = (stmt: StatementSync): string => {
+  const summary: Summary = parseSummaryData(stmt)
+  const values = Object.entries(summary.playerTypes ?? {}).map(
+    ([name, count]) => ({ pt: name, c: count }),
+  )
+
+  return JSON.stringify(
+    {
+      title: 'Clients',
+      description: 'Distribution of clients used',
+      data: {
+        values: values,
+      },
+      ...genPie('container', 420, 'pt', 'Client'),
+    },
+  )
+}
 const numInstanceLine = (stmt: StatementSync): string => {
   const values = [{}]
   for (const row of stmt.iterate()) {
@@ -74,13 +166,14 @@ const numInstanceLine = (stmt: StatementSync): string => {
   }
   return JSON.stringify({
     $schema: vegaSchema,
+    background: null,
     title: 'Number of Instances Over Time',
     description: 'Number of instances of the server over time, by version',
     data: {
       values: values,
     },
-    height: 500,
-    width: 1000,
+    height: 420,
+    width: 'container',
     mark: {
       type: 'line',
       tooltip: true,
@@ -88,6 +181,10 @@ const numInstanceLine = (stmt: StatementSync): string => {
     },
     encoding: {
       color: {
+        legend: {
+          labelLimit: 250,
+          symbolLimit: 13,
+        },
         field: 'v',
         type: 'nominal',
         sort: {
@@ -110,141 +207,6 @@ const numInstanceLine = (stmt: StatementSync): string => {
       },
     },
   })
-}
-
-const musicFsPie = (stmt: StatementSync): string => {
-  const summary: Summary = parseSummaryData(stmt)
-  const values = Object.entries(summary.musicFS ?? {}).map(
-    ([type, count]) => ({ fs: type, c: count }),
-  )
-
-  return JSON.stringify(
-    {
-      $schema: vegaSchema,
-      title: 'Music File Systems',
-      description: 'Distribution of file systems used for music files',
-      data: {
-        values: values,
-      },
-      mark: {
-        type: 'arc',
-        tooltip: true,
-      },
-      encoding: {
-        theta: {
-          field: 'c',
-          type: 'quantitative',
-          stack: 'normalize',
-          title: 'Percentage',
-        },
-        color: {
-          field: 'fs',
-          type: 'nominal',
-          sort: {
-            field: 'c',
-            order: 'descending',
-          },
-          title: 'File System (music)',
-        },
-        order: {
-          field: 'c',
-          type: 'quantitative',
-          sort: 'descending',
-          title: 'Count',
-        },
-      },
-    },
-  )
-}
-
-const dataFsPie = (stmt: StatementSync): string => {
-  const summary: Summary = parseSummaryData(stmt)
-  const values = Object.entries(summary.dataFS ?? {}).map(
-    ([type, count]) => ({ fs: type, c: count }),
-  )
-
-  return JSON.stringify(
-    {
-      $schema: vegaSchema,
-      title: 'Data File Systems',
-      description: 'Distribution of file systems used for data files',
-      data: {
-        values: values,
-      },
-      mark: {
-        type: 'arc',
-        tooltip: true,
-      },
-      encoding: {
-        theta: {
-          field: 'c',
-          type: 'quantitative',
-          stack: 'normalize',
-          title: 'Percentage',
-        },
-        color: {
-          field: 'fs',
-          type: 'nominal',
-          sort: {
-            field: 'c',
-            order: 'descending',
-          },
-          title: 'File System (data)',
-        },
-        order: {
-          field: 'c',
-          type: 'quantitative',
-          sort: 'descending',
-          title: 'Count',
-        },
-      },
-    },
-  )
-}
-
-const playerTypePie = (stmt: StatementSync): string => {
-  const summary: Summary = parseSummaryData(stmt)
-  const values = Object.entries(summary.playerTypes ?? {}).map(
-    ([name, count]) => ({ pt: `${name}: ${count}`, c: count }),
-  )
-
-  return JSON.stringify(
-    {
-      $schema: vegaSchema,
-      title: 'Player Types',
-      description: 'Distribution of clients used',
-      data: {
-        values: values,
-      },
-      mark: {
-        type: 'arc',
-        tooltip: true,
-      },
-      encoding: {
-        theta: {
-          field: 'c',
-          type: 'quantitative',
-          stack: 'normalize',
-          title: 'Percentage',
-        },
-        color: {
-          field: 'pt',
-          type: 'nominal',
-          sort: {
-            field: 'c',
-            order: 'descending',
-          },
-          title: 'Client',
-        },
-        order: {
-          field: 'c',
-          type: 'quantitative',
-          sort: 'descending',
-          title: 'Count',
-        },
-      },
-    },
-  )
 }
 
 export { dataFsPie, musicFsPie, numInstanceLine, osPie, playerTypePie }
